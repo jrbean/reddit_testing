01221e86e6a82811b6c10b2523ec7b25e2034d12
Justin Bean <beanjustinr@gmail.com>
2015-11-01 20:43:02 -0500

    flesh out comment controller and model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.create!(comment_params)
+    redirect_to :back
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def destroy
+    @comment = Comment.find params[:id]
+  end
+
+  def edit
+    @comment = Comment.find params[:id]
+  end
+
+  def show
+    @comment = Comment.find params[:id]
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,11 @@ class Comment < ActiveRecord::Base
   has_many :votes, as: :voteable
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  def vote_count
+    uc = self.votes.where(status: true).count
+    dc = self.votes.where(status: false).count
+    uc - dc
+  end
+  
 end
